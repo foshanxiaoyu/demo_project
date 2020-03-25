733feef2441ecc5e917313d82f701c11a4fd0f7a
foshanxiaoyu <1422054416@qq.com>
2020-03-25 16:48:19 +0800

    202003251648

---
+++ DAYONETRY.java
@@ -0,0 +1,11 @@
+package demo_project;
+
+/**
+ * DAYONETRY
+ */
+public class DAYONETRY {
+    public static void main(final String[] args) {
+        final var name = "tom and jerniy";
+        System.out.println(name);
+    }
+}
